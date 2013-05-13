9243fc16a78826d3a9b9ed082350248289f0016b
Seivan Heidari <seivan@kth.se>
2013-05-13 07:41:35 +0200

    Update pods

--- Example/Pods/Local Podspecs/SHOmniAuth.podspec
+++ Example/Pods/Local Podspecs/SHOmniAuth.podspec
@@ -8,8 +8,8 @@
 #
 Pod::Spec.new do |s|
   s.name         = "SHOmniAuth"
-  s.version      = "0.0.1"
-  s.summary      = "Pattern for Authentication via different providers"
+  s.version      = "0.1.1"
+  s.summary      = "Pattern for Authentication via different providers."
   s.description  = <<-DESC
                     Fixed pattern for authentication via different providers
   
@@ -22,10 +22,10 @@ Pod::Spec.new do |s|
                     Ask the users to pick from a list of accounts to auth or add a new account. 
                    DESC
   s.homepage     = "https://github.com/seivan/SHOmniAuth"
-  s.license      = {:type => 'MIT' } #,:file => 'LICENSE.md'
+  s.license      = {:type => 'MIT' } 
   s.author       = { "Seivan Heidari" => "seivan.heidari@icloud.com" }
   
-  s.source       = { :git => "https://github.com/seivan/SHAccountStore.git"} #, :tag => "0.0.1"}
+  s.source       = { :git => "https://github.com/seivan/SHOmniAuth.git", :tag => s.version.to_s }
   
 
   s.platform  = :ios, "5.0"
---
+++ Example/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// BlocksKit
+#define COCOAPODS_POD_AVAILABLE_BlocksKit
+#define COCOAPODS_VERSION_MAJOR_BlocksKit 1
+#define COCOAPODS_VERSION_MINOR_BlocksKit 8
+#define COCOAPODS_VERSION_PATCH_BlocksKit 2
+
+// LUKeychainAccess
+#define COCOAPODS_POD_AVAILABLE_LUKeychainAccess
+#define COCOAPODS_VERSION_MAJOR_LUKeychainAccess 1
+#define COCOAPODS_VERSION_MINOR_LUKeychainAccess 0
+#define COCOAPODS_VERSION_PATCH_LUKeychainAccess 1
+
+// SHOmniAuth
+#define COCOAPODS_POD_AVAILABLE_SHOmniAuth
+#define COCOAPODS_VERSION_MAJOR_SHOmniAuth 0
+#define COCOAPODS_VERSION_MINOR_SHOmniAuth 1
+#define COCOAPODS_VERSION_PATCH_SHOmniAuth 1
+
+// libffi
+#define COCOAPODS_POD_AVAILABLE_libffi
+#define COCOAPODS_VERSION_MAJOR_libffi 3
+#define COCOAPODS_VERSION_MINOR_libffi 0
+#define COCOAPODS_VERSION_PATCH_libffi 11
+
